ef192d6ee7a0c202293402d7a744eca5a8cb80e1
iKapitonau <ilya.th.kapitonov@gmail.com>
2020-04-27 12:04:55 +0300

    Add get_cpu_stat() for cpu usage monitoring

--- scand.c
+++ scand.c
@@ -1,3 +1,4 @@
+#include <inttypes.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdio.h>
@@ -5,7 +6,8 @@
 #include <syslog.h>
 #include <unistd.h>
 
-#define DELAY_MS	1000
+#define DELAY_S	5
+#define STAT_ENTRIES_NUMBER 10
 
 static void init(void)
 {
@@ -26,9 +28,40 @@ static void init(void)
 	if (chdir("/") < 0)
 		exit(EXIT_FAILURE);
 
+	
 	close(STDERR_FILENO);
 	close(STDOUT_FILENO);
 	close(STDIN_FILENO);
+}
+
+static double get_cpu_stat(void)
+{
+	static uint64_t prev_cpu_times[STAT_ENTRIES_NUMBER];
+	static uint64_t cpu_times[STAT_ENTRIES_NUMBER];
+	static uint64_t prev_sum;
+
+	uint64_t sum = 0;
+
+	FILE *f_stat = fopen("/proc/stat", "r");
+
+	// skip 'cpu ' in /proc/stat
+	fseek(f_stat, 4, SEEK_SET);	
+
+	for (size_t i = 0; i < STAT_ENTRIES_NUMBER; ++i) {
+		fscanf(f_stat, "%"SCNu32, &cpu_times[i]); 
+		sum += cpu_times[i];
+	}
+
+	fclose(f_stat);
+	
+	uint64_t diff_sum = sum - prev_sum;
+	uint64_t diff_idle = (cpu_times[3] + cpu_times[4]) - (prev_cpu_times[3] + prev_cpu_times[4]);
+
+	prev_sum = sum;
+	for (size_t i = 0; i < STAT_ENTRIES_NUMBER; ++i)
+		prev_cpu_times[i] = cpu_times[i];
+
+	return (double)(diff_sum - diff_idle) / diff_sum * 100;
 }
 
 static void term(void)
@@ -41,8 +74,8 @@ void main(void)
 	init();
 
 	while (1) {
-
-		sleep(DELAY_MS);
+		syslog(LOG_DEBUG, "%lf\n", get_cpu_stat());
+		sleep(DELAY_S);
 	}
 
 	term();
