9e0295a82858e5430a1654b653b003bb9cebf656
iKapitonau <ilya.th.kapitonov@gmail.com>
2020-05-17 11:18:26 +0300

    Add header

--- scan.py
+++ scan.py
@@ -7,6 +7,8 @@ if __name__ == '__main__':
 	libname = './scand.so'
 	c_lib = CDLL(libname)
 
+	print('cpu_useful', 'cpu_idle', 'cpu_iowait', 'mem_swap_total', 'mem_swap_free', 'mem_ram_total',
+		'mem_ram_free', 'disk_opcount', 'disk_iotime', sep=',')
 	while True:
 		print(c_lib.get_cpu_useful(), c_lib.get_cpu_idle(), c_lib.get_cpu_iowait(),
 			c_lib.get_mem_swap_total(), c_lib.get_mem_swap_free(),
